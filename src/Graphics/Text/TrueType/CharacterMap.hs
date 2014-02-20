5e9a7353f432551ad563fe27fd9b9c4c9b47cea6
Vincent Berthoux <vincent.berthoux@gmail.com>
2014-02-20 14:06:42 +0100

    Preparing glyph search.

--- src/Graphics/Text/TrueType/CharacterMap.hs
+++ src/Graphics/Text/TrueType/CharacterMap.hs
@@ -1,4 +1,10 @@
-module Graphics.Text.TrueType.CharacterMap where
+{-# LANGUAGE RankNTypes #-}
+module Graphics.Text.TrueType.CharacterMap
+    ( TtfEncoding( .. )
+    , CharacterMaps
+    , LangId
+    , findCharGlyph
+    ) where
 
 import Control.Monad( replicateM, forM )
 import Control.Applicative( (<$>), (<*>) )
@@ -13,9 +19,12 @@ import Data.Binary.Get( Get
 
 import Data.Binary.Put( putWord16be
                       , putWord32be )
-import qualified Data.Map as M
 import Data.Int( Int16 )
+import Data.List( find, sortBy )
+import qualified Data.Map as M
+import Data.Maybe( fromMaybe )
 import Data.Word( Word8, Word16, Word32 )
+import Data.Ord( comparing )
 
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as VU
@@ -23,6 +32,9 @@ import qualified Data.Vector.Unboxed as VU
 {-import Debug.Trace-}
 {-import Text.Printf-}
 
+--------------------------------------------------
+----            TtfEncoding
+--------------------------------------------------
 data TtfEncoding
   = EncodingSymbol
   | EncodingUnicode
@@ -54,12 +66,28 @@ instance Binary TtfEncoding where
         6 -> return EncodingJohab
         _ -> fail "Unknown encoding"
 
+class CharMappeable a where
+  -- | Given a group a character, return a valid glyph
+  -- id and the rest of the string.
+  glyphIdFromTable :: a -> Char -> Int
+
+  -- | Given a table, retrieve the language code
+  langIdOfCharMap :: a -> LangId
+
+type LangId = Word16
+
+--------------------------------------------------
+----            CharacterMaps
+--------------------------------------------------
 data CharacterMap = CharacterMap
     { _charMapPlatformId       :: !Word16
     , _charMapPlatformSpecific :: !Word16
     , _charMap                 :: !CharacterTable
     }
     deriving (Eq, Show)
+
+instance Ord CharacterMap where
+    compare = comparing _charMap
 
 newtype CharacterMaps = CharacterMaps [CharacterMap]
     deriving (Eq, Show)
@@ -82,7 +110,7 @@ instance Binary CharacterMaps where
          when (toSkip > 0)
               (skip $ fromIntegral toSkip)
          CharacterMap platformId platformSpecific <$> get
-    return $ CharacterMaps tables
+    return . CharacterMaps $ sortBy (comparing _charMap) tables
 
 data CharMapOffset = CharMapOffset
     { _cmoPlatformId :: !Word16
@@ -96,11 +124,83 @@ instance Binary CharMapOffset where
     put (CharMapOffset platform encoding offset) =
       putWord16be platform >> put encoding >> putWord32be offset
 
+--------------------------------------------------
+----            CharacterTable
+--------------------------------------------------
+data CharacterTable
+    = TableFormat0 !Format0
+    | TableFormat2 !Format2
+    | TableFormat4 !Format4
+    | TableFormat6 !Format6
+    | TableFormatUnknown !Word16
+    deriving (Eq, Show)
+
+charTableMap :: (forall table . (CharMappeable table) => table -> a)
+             -> CharacterTable -> a
+charTableMap f = go
+  where
+    go (TableFormat0 t) = f t
+    go (TableFormat2 t) = f t
+    go (TableFormat4 t) = f t
+    go (TableFormat6 t) = f t
+    go (TableFormatUnknown v) = f v
+
+findCharGlyph :: CharacterMaps -> LangId -> Char -> Int
+findCharGlyph (CharacterMaps charMaps) langId character =
+    fromMaybe 0 $ find (/= 0)
+        [charTableMap (flip glyphIdFromTable character) m
+                | allMap <- charMaps
+                , let m = _charMap allMap
+                , isLangCompatible m]
+  where
+    isLangCompatible v = tableLang == 0 || tableLang == langId 
+      where tableLang = charTableMap langIdOfCharMap v
+
+instance Ord CharacterTable where
+    compare (TableFormat0 v1) (TableFormat0 v2) =
+        (comparing langIdOfCharMap) v1 v2
+    compare (TableFormat2 v1) (TableFormat2 v2) =
+        (comparing langIdOfCharMap) v1 v2
+    compare (TableFormat4 v1) (TableFormat4 v2) =
+        (comparing langIdOfCharMap) v1 v2
+    compare (TableFormat6 v1) (TableFormat6 v2) =
+        (comparing langIdOfCharMap) v1 v2
+    compare (TableFormat0 _) _ = LT
+    compare (TableFormat2 _) _ = LT
+    compare (TableFormat4 _) _ = LT
+    compare (TableFormat6 _) _ = LT
+    compare _ _ = GT
+
+instance Binary CharacterTable where
+    put _ = fail "Binary.put CharacterTable - Unimplemented"
+    get = do
+      format <- getWord16be
+      case format of
+        0 -> TableFormat0 <$> get
+        2 -> TableFormat2 <$> get
+        4 -> TableFormat4 <$> get
+        6 -> TableFormat6 <$> get
+        n -> return $ TableFormatUnknown n
+
+instance CharMappeable Word16 where
+  glyphIdFromTable _ _ = 0
+  langIdOfCharMap _ = 0
+
+
+--------------------------------------------------
+----            Format4
+--------------------------------------------------
 data Format4 = Format4
-    { _f4Language :: {-# UNPACK #-} !Word16
+    { _f4Language :: {-# UNPACK #-} !LangId
     , _f4Map :: M.Map Word16 Word16
     }
     deriving (Eq, Show)
+
+instance CharMappeable Format4 where
+  glyphIdFromTable tab v =
+      fromIntegral . M.findWithDefault 0 wc $ _f4Map tab
+        where wc = fromIntegral $ fromEnum v
+  langIdOfCharMap = _f4Language
 
 instance Binary Format4 where
   put _ = error "put Format4 - unimplemented"
@@ -159,21 +259,40 @@ instance Binary Format4 where
               , let glyphId = indexTable VU.! index
               ]
 
-data CharacterTable
-    = TableFormat0 Word16 !(VU.Vector Word8)
-    | TableFormat2 !Format2
-    | TableFormat4 !Format4
-    | TableFormat6 !Format6
-    | TableFormatUnknown !Word16
+--------------------------------------------------
+----            Format0
+--------------------------------------------------
+data Format0 = Format0
+    { _format0Language :: {-# UNPACK #-} !LangId
+    , _format0Table    :: !(VU.Vector Word8)
+    }
     deriving (Eq, Show)
 
-getFormat0 :: Get CharacterTable
-getFormat0 = do
-    tableSize <- getWord16be
-    when (tableSize /= 262) $
-          fail "table cmap format 0 : invalid size"
-    lang <- getWord16be
-    TableFormat0 lang <$> VU.replicateM 256 getWord8
+instance CharMappeable Format0 where
+  glyphIdFromTable Format0 { _format0Table = table } v
+    | ic > VU.length table = 0
+    | otherwise = fromIntegral $ table VU.! ic
+        where ic = fromEnum v
+        
+  langIdOfCharMap = _format0Language
+
+instance Binary Format0 where
+    put _ = fail "Binary.Format0.put - unimplemented"
+    get = do
+        tableSize <- getWord16be
+        when (tableSize /= 262) $
+            fail "table cmap format 0 : invalid size"
+        Format0 <$> getWord16be <*> VU.replicateM 256 getWord8
+
+--------------------------------------------------
+----            Format2
+--------------------------------------------------
+data Format2 = Format2
+    { _format2Language   :: {-# UNPACK #-} !LangId
+    , _format2SubKeys    :: !(VU.Vector Word16)
+    , _format2SubHeaders :: !(V.Vector Format2SubHeader)
+    }
+    deriving (Eq, Show)
 
 data Format2SubHeader = Format2SubHeader
     { _f2SubCode       :: {-# UNPACK #-} !Word16
@@ -182,6 +301,10 @@ data Format2SubHeader = Format2SubHeader
     , _f2IdRangeOffset :: {-# UNPACK #-} !Word16
     }
     deriving (Eq, Show)
+
+instance CharMappeable Format2 where
+  glyphIdFromTable _ _ = 0
+  langIdOfCharMap = _format2Language
 
 instance Binary Format2SubHeader where
     put (Format2SubHeader a b c d) =
@@ -192,6 +315,7 @@ instance Binary Format2SubHeader where
 
     get = Format2SubHeader <$> g16 <*> g16 <*> (fromIntegral <$> g16) <*> g16
       where g16 = getWord16be
+
 
 instance Binary Format2 where
     put _ = fail "Format2.put - unimplemented"
@@ -204,19 +328,23 @@ instance Binary Format2 where
       -- TODO finish the parsing of format 2
       return $ Format2 lang subKeys subHeaders
 
-data Format2 = Format2
-    { _format2Language   :: {-# UNPACK #-} !Word16
-    , _format2SubKeys    :: !(VU.Vector Word16)
-    , _format2SubHeaders :: !(V.Vector Format2SubHeader)
-    }
-    deriving (Eq, Show)
-
+--------------------------------------------------
+----            Format6
+--------------------------------------------------
 data Format6 = Format6
-    { _format6Language   :: {-# UNPACK #-} !Word16
+    { _format6Language   :: {-# UNPACK #-} !LangId
     , _format6FirstCode  :: {-# UNPACK #-} !Word16
     , _format6ArrayIndex :: !(VU.Vector Word16)
     }
     deriving (Eq, Show)
+
+instance CharMappeable Format6 where
+  glyphIdFromTable Format6 { _format6ArrayIndex = table } v
+    | ic < VU.length table = fromIntegral $ table VU.! ic
+    | otherwise = 0
+      where ic = fromEnum v
+
+  langIdOfCharMap = _format6Language
 
 instance Binary Format6 where
     put _ = fail "Format6.put - unimplemented"
@@ -226,15 +354,3 @@ instance Binary Format6 where
         firstCode <- getWord16be
         entryCount <- fromIntegral <$> getWord16be
         Format6 language firstCode <$> VU.replicateM entryCount getWord16be
-
-instance Binary CharacterTable where
-    put _ = fail "Binary.put CharacterTable - Unimplemented"
-    get = do
-      format <- getWord16be
-      case format of
-        0 -> getFormat0
-        2 -> TableFormat2 <$> get
-        4 -> TableFormat4 <$> get
-        6 -> TableFormat6 <$> get
-        n -> return $ TableFormatUnknown n
-
